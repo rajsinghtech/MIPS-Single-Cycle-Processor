b179035d4e1dd68963234fe6906d5a678f4a5d7f
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-10-23 01:04:39 -0500

    More Barrel Shifting

--- Components/Fetch/barrel_shifter.vhd
+++ Components/Fetch/barrel_shifter.vhd
@@ -24,10 +24,10 @@ use IEEE.Numeric_Std.all;
 use work.Data_Types.all;
 
 entity barrel_shifter is
-  generic(MAX_SHIFT: integer := 5; WORD_SIZE : integer := 32; SHIFT_TYPE_BITS: integer := 2);
+  generic(MAX_SHIFT: integer := 5; WORD_SIZE : integer := 32);
   port (
 	i_src : in std_logic_vector( WORD_SIZE - 1 downto 0 );
-	i_shift_type: in std_logic_vector( SHIFT_TYPE_BITS - 1 downto 0);
+	i_shift_type: in std_logic;
 	i_shamt: in std_logic_vector( MAX_SHIFT - 1 downto 0 );
 	o_shift_out : out std_logic_vector( WORD_SIZE - 1 downto 0 )
   );
@@ -35,44 +35,62 @@ end barrel_shifter;
 
 architecture structure of barrel_shifter is
 
-	component mux2t1_N is
+	component mux4t1 is
 		generic(N : integer := 32);
-		port(i_S          : in std_logic;
-			i_D0         : in std_logic_vector(WORD_SIZE - 1 downto 0);
-			i_D1         : in std_logic_vector(WORD_SIZE - 1 downto 0);
-			o_O          : out std_logic_vector(WORD_SIZE - 1 downto 0));
+		port(i_S          : in std_logic_vector( 1 downto 0);
+			i_D0         : in std_logic;
+			i_D1         : in std_logic;
+			i_D2         : in std_logic;
+			i_D3         : in std_logic;
+			o_O          : out std_logic);
 	end component;
+
+  signal shift_layer_data : shift_layer(MAX_SHIFT - 1 downto 0);
   
-  signal sll_res : std_logic_vector( WORD_SIZE - 1 downto 0 );
-  signal srl_res : std_logic_vector( WORD_SIZE - 1 downto 0 );
-  signal sra_res : std_logic_vector( WORD_SIZE - 1 downto 0 );
-  signal sll_srl_out : std_logic_vector( WORD_SIZE - 1 downto 0);
   
 begin
 
---	sll_res <= std_logic_vector(to_unsigned(to_integer(unsigned(i_src)) <<  to_integer(unsigned(i_shamt)) ), sll_res'length);
---	srl_res <= std_logic_vector(to_unsigned(to_integer(unsigned(i_src)) >> to_integer(unsigned(i_shamt)) ), sll_res'length);
---	sra_res <= std_logic_vector(to_signed(to_integer(signed(i_src)) >> to_integer(signed(i_shamt)) ), sll_res'length);
+  G_SHIFT_LAYER: for i in 0 to MAX_SHIFT-1 generate
+	G_SHIFT_MUX: for j in 0 to WORD_SIZE-1 generate
+		SHIFT_RIGHT_START : IF j + 2 ** i > WORD_SIZE-1 generate
 
-	sll_res(WORD_SIZE - 1 downto to_integer(unsigned(i_shamt))) <= (i_src( WORD_SIZE - 1 - to_integer(unsigned(i_shamt)) downto 0), others => '0');
+			MUX: mux4t1 port map( 
+				i_S => ( 0 => i_shamt(i), 1 => i_shift_type),
+				i_D0 => shift_layer_data(i,j),
+				i_D1 => shift_layer_data(i, j - (2 ** i)),
+				i_D2 => '0',
+				i_D3 => '1',
+				o_O => shift_layer_data(i, j));
 
-	srl_res(WORD_SIZE - 1 - to_integer(unsigned(i_shamt)) downto 0) <= (i_src( WORD_SIZE - 1 downto to_integer(unsigned(i_shamt))), others => '0');
+		end generate SHIFT_RIGHT_START;
 
-	srl_res(WORD_SIZE - 1 - to_integer(unsigned(i_shamt)) downto 0) <= (i_src( WORD_SIZE - 1 downto to_integer(unsigned(i_shamt))), others => '1');
+		SHIFT_LEFT_END : if j - 2 ** i < 0 generate
 
+			MUX: mux4t1 port map( 
+				i_S => ( 0 => i_shamt(i), 1 => i_shift_type),
+				i_D0 => shift_layer_data(i,j),
+				i_D1 => '0',
+				i_D2 => shift_layer_data(i, j + (2 ** i)),
+				i_D3 => shift_layer_data(i, j + (2 ** i)),
+				o_O => shift_layer_data(i, j));
+		
+		end generate SHIFT_LEFT_END;
 
-	  Shift_LL_Sel: mux2t1_N
-	  port map(
-				i_S     => i_shift_type(0),
-				i_D0    => sll_res,
-				i_D1    => srl_res,
-				o_O     => sll_srl_out);
-				
-	  Shift_LA_Sel: mux2t1_N
-	  port map(
-				i_S     => i_shift_type(1),
-				i_D0    => sll_srl_out,
-				i_D1    => sra_res,
-				o_O     => o_shift_out);
+		NORMAL_SHIFT : if j + 2 ** i < WORD_SIZE-1 AND j - 2 ** i > 0 generate
+
+		MUX: mux4t1 port map( 
+			i_S => ( 0 => i_shamt(i), 1 => i_shift_type),
+			i_D0 => shift_layer_data(i,j),
+			i_D1 => shift_layer_data(i, j - (2 ** i)),
+			i_D2 => shift_layer_data(i, j + (2 ** i)),
+			i_D3 => shift_layer_data(i, j + (2 ** i)),
+			o_O => shift_layer_data(i, j));
+
+		end generate NORMAL_SHIFT;
+	
+	end generate G_SHIFT_MUX;
+  end generate G_SHIFT_LAYER;
+
+	
 
 end structure;
