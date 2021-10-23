84660ef3661b31dfb9fba098fc57fec92bfd7fe3
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-10-23 01:29:53 -0500

    Please, its so close and it is 1:30 AM

--- Components/Fetch/barrel_shifter.vhd
+++ Components/Fetch/barrel_shifter.vhd
@@ -36,7 +36,6 @@ end barrel_shifter;
 architecture structure of barrel_shifter is
 
 	component mux4t1 is
-		generic(N : integer := 32);
 		port(i_S          : in std_logic_vector( 1 downto 0);
 			i_D0         : in std_logic;
 			i_D1         : in std_logic;
