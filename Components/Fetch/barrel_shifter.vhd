95e7fbbb1cc030c3082f70d00121ad720abe8524
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-10-22 22:57:37 -0500

    Update barrel_shifter.vhd

--- Components/Fetch/barrel_shifter.vhd
+++ Components/Fetch/barrel_shifter.vhd
@@ -50,10 +50,10 @@ architecture structure of barrel_shifter is
   
 begin
 
-	sll_res <= std_logic_vector(unsigned(i_src) * (2 ** to_integer(unsigned(i_shamt))));
-	srl_res <= std_logic_vector(unsigned(i_src) / (2 ** to_integer(unsigned(i_shamt))));
-	sra_res <= std_logic_vector(signed(i_src) / (2 ** to_integer(signed(i_shamt))));
-	
+	sll_res <= std_logic_vector(to_unsigned(to_integer(unsigned(i_src)) * ( 2 ** to_integer(unsigned(i_shamt)) ), sll_res'length));
+	srl_res <= std_logic_vector(to_unsigned(to_integer(unsigned(i_src)) / ( 2 ** to_integer(unsigned(i_shamt)) ), sll_res'length));
+	sra_res <= std_logic_vector(to_signed(to_integer(signed(i_src)) / ( 2 ** to_integer(signed(i_shamt)) ), sll_res'length));
+
 	  Shift_LL_Sel: mux2t1_N
 	  port map(
 				i_S     => i_shift_type(0),
