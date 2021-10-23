61443771da976aa01e7613934997104671eb7023
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-10-22 21:42:06 -0500

    Fixes to Fetch Logic
    Move memory module out of fetch module

--- Components/Fetch/Fetch.vhd
+++ Components/Fetch/Fetch.vhd
@@ -23,18 +23,19 @@ use IEEE.Numeric_Std.all;
 
 use work.Data_Types.all;
 
-entity instruction_decoder is
+entity fetch_logic is
   generic(J_TYPE_LEN: integer := 4; IMMEDIATE_LEN: integer := 16; ADDR_LEN: integer := 32; WORD_SIZE: integer := 32);
   port (
-	i_imm : in std_logic_vector( IMMEDIATE_LEN - 1 downto 0 );
-	i_addr: in std_logic_vector( ADDR_LEN - 1 downto 0 );
-	i_clk : in std_logic;
-	j_type: in std_logic_vector( J_TYPE_LEN - 1 downto 0);
-	o_inst : out std_logic_vector( WORD_SIZE - 1 downto 0 );
+    i_imm : in std_logic_vector( IMMEDIATE_LEN - 1 downto 0 );
+    i_addr: in std_logic_vector( ADDR_LEN - 1 downto 0 );
+    instruction : in std_logic_vector( ADDR_LEN - 1 downto 0);
+    i_clk : in std_logic;
+    j_type: in std_logic_vector( J_TYPE_LEN - 1 downto 0);
+    o_inst : out std_logic_vector( WORD_SIZE - 1 downto 0 )
   );
-end instruction_decoder;
+end fetch_logic;
 
-architecture structure of instruction_decoder is
+architecture structure of fetch_logic is
   
 
   component mem
@@ -76,7 +77,6 @@ architecture structure of instruction_decoder is
   signal next_instruction : std_logic_vector( ADDR_LEN - 1 downto 0);
   
   signal jump_address : std_logic_vector( ADDR_LEN - 1 downto 0);
-  signal instruction : std_logic_vector( ADDR_LEN - 1 downto 0);
   
   signal branch_address : std_logic_vector( WORD_SIZE - 1 downto 0) := to_stdlogicvector(x"00000000");
   signal branch_immediate : std_logic_vector( WORD_SIZE - 1 downto 0);
@@ -86,39 +86,30 @@ architecture structure of instruction_decoder is
   signal branch_jump : std_logic_vector( WORD_SIZE - 1 downto 0);
   
 begin
+  
   branch_immediate(17 downto 2) <= i_imm(15 downto 0);
   
-  
-
   PC: dffg_N
   port map(
     i_CLK => i_clk,
-	i_RST => '1',
-	i_WE => '1',
-	i_D => next_address,
-	o_Q => program_counter
+    i_RST => '0',
+    i_WE => '1',
+    i_D => next_address,
+    o_Q => program_counter
   );
 
   program_plus_four: Ripple_Adder
   port map(
             i_A    => instruction_offset,
-			i_B    => program_counter,
+		      	i_B    => program_counter,
             o_S    => next_instruction);
 
 
   branch_address: Ripple_Adder
   port map(
             i_A    => next_instruction,
-			i_B    => branch_immediate,
+		      	i_B    => branch_immediate,
             o_S    => branch_address);
-
-
-  dmem: mem 
-  port map(data => (others => '0'), 
-           we => '0',
-           clk => i_clk,
-           addr => program_counter,
-           q => instruction);
 		   
 	jump_address( 31 downto 28) <= program_counter( 31 downto 28);
 	jump_address( 27 downto 2) <= instruction( 25 downto 0);
