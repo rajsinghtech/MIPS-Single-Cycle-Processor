41b2671d6a0618371cc9d702980006871343a033
Jordan-Mcgh <56053221+Jordan-Mcgh@users.noreply.github.com>
2021-10-27 22:51:34 -0500

    Descriptions

--- Components/ALU/Data_Types.vhd
+++ Components/ALU/Data_Types.vhd
@@ -10,16 +10,66 @@ package Data_Types is
 	type DATA_FIELD is array(integer range <>) of std_logic_vector(31 downto 0);
 	type shift_layers is array(5 downto 0, 31 downto 0) of std_logic;
 	
-	type OP_CODE is ( add, addi, lw, sw);
+	type ALU_ENCODING is ( op_add, op_sub, op_and, op_or, op_nor, op_xor, op_sll, op_srl, op_sra, op_quad, op_lui);
+	type OP_CODE is ( r_type, lwc, swc, beqc, bnec, jc, jalc);
+	type FUNC_CODE is ( addc, addic, addiuc, adduc, andc, andic, luic, norc, xorc, xori, orc, oric, sltc, sltic, sllc, srlc, srac,subc, subuc, jrc, quadc);
 	
+
+	type ALUENCODING_ARRAY is array(OP_CODE) of std_logic_vector(5 downto 0);
 	type OPCODE_ARRAY is array(OP_CODE) of std_logic_vector(5 downto 0);
+	type FUNCCODE_ARRAY is array(OP_CODE) of std_logic_vector(5 downto 0);
 	
+	constant DECODE_ALU_ENCODING : ALUENCODING_ARRAY;
 	constant DECODE_OP : OPCODE_ARRAY;
+	constant DECODE_FUNC : FUNCCODE_ARRAY;
 
 end package Data_Types;
 
 package body Data_Types is
 
-	constant DECODE_OP : OPCODE_ARRAY := ( add => "100000", addi => "111000", lw => "111001", sw => "011010" );
+	constant DECODE_OP : OPCODE_ARRAY := (  r_type => "000000",
+											lwc => "100011",
+											swc => "101011", 
+											beqc => "000100", 
+											bnec => "000101", 
+											jc => "000010", 
+											jalc => "000011" );
+
+	constant DECODE_OP : OPCODE_ARRAY := (  op_add => "000000",
+											op_sub => "100000",
+											op_and => "000001", 
+											op_or  => "000010",
+											op_nor => "000011", 
+											op_xor => "000100", 
+											op_sll => "001101", 
+											op_srl => "010101", 
+											op_sra => "011101", 
+											op_quad => "000110", 
+											op_lui => "000111");
+
+	constant DECODE_FUNC : FUNCCODE_ARRAY := (  
+												addc => "100000", 
+												addic => "001000", 
+												addiuc => "001001",
+												adduc => "100001", 
+												andc => "100100", 
+												andic => "001100", 
+												luic => "001111", 
+												norc => "100111", 
+												xorc => "100110", 
+												xori => "001110", 
+												orc ==> "100101", 
+												oric => "001101", 
+												sltc => "101010", 
+												sltic => "001010", 
+												sllc => "000000", 
+												srlc => "000010", 
+												srac => "000011",
+												subc => "100010", 
+												subuc => 100011, 
+												jrc => "001000", 
+												quadc => "011111"
+	 );
+											
 
 end package body Data_Types;
--- Components/Fetch/Decode.vhd
+++ Components/Fetch/Decode.vhd
@@ -24,7 +24,7 @@ use IEEE.Numeric_Std.all;
 use work.Data_Types.all;
 
 entity decode_logic is
-  generic(WORD_SIZE : integer := 32; ALU_OPERATIONS : integer := 8; MAX_SHIFT : integer := 5);
+  generic(WORD_SIZE : integer := 32; ALU_OPERATIONS : integer := 8; MAX_SHIFT : integer := 5; OP_CODE_SIZE : integer := 6; FUNC_CODE_SIZE : integer := 6);
   port (
       i_instruction : in std_logic_vector( WORD_SIZE - 1 downto 0 );
       o_jump : out std_logic;
@@ -35,110 +35,60 @@ entity decode_logic is
       o_jumpIns : out std_logic;
       o_regWrite : out std_logic;
       o_shamt : out std_logic_vector( MAX_SHIFT - 1 downto 0);
-      o_link : out std_logic
+      o_link : out std_logic,
+      o_bne : out std_logic
     );
 end decode_logic;
 
+  -- Instruction format
+  -- (31 downto 26) = opcode
+  -- (25 downto 21) = rs
+  -- (20 downto 16) = rt
+  -- (15 downto 11) = rd
+  -- (10 downto 6) = shamt
+  -- (5 downto 0) = func
+
+  -- (31 downto 26) = opcode
+  -- (25 downto 21) = rs
+  -- (20 downto 16) = rt
+  -- (15 downto 0) = immediate
+
+  -- (31 downto 26) = opcode
+  -- (25 downto 0) = jump address
+  
 architecture structure of decode_logic is
+
+  signal op_code : std_logic_vector(OP_CODE_SIZE - 1 downto 0);
+  signal func_code : std_logic_vector(FUNC_CODE_SIZE - 1 downto 0);
+
+  begin
+
+  op_code <= i_instruction(31 downto 26);
+  func_code <= i_instruction(5 downto 0);
+
+  o_jump <= '1' when op_code = DECODE_OP(jc)
+                else '1' when op_code = DECODE_OP(jalc)
+                else '0';
   
+  o_branch <= '1' when op_code = DECODE_OP(beqc)
+                else '1' when op_code = DECODE_OP(bnec)
+                else '0';
 
-  component mem
-  port(
-	clk		: in std_logic;
-	addr	        : in std_logic_vector((ADDR_LEN-1) downto 0);
-	data	        : in std_logic_vector((WORD_SIZE-1) downto 0);
-	we		: in std_logic := '1';
-	q		: out std_logic_vector((WORD_SIZE -1) downto 0));
-  end component;
-
-  component Ripple_Adder is
-    port(i_A : in std_logic_vector(WORD_SIZE - 1 downto 0);
-	     i_B : in std_logic_vector(WORD_SIZE - 1 downto 0);
-	     o_S : out std_logic_vector(WORD_SIZE - 1 downto 0));
-  end component;
-
-  component mux2t1_N is
-    generic(N : integer := 32);
-    port(i_S          : in std_logic;
-         i_D0         : in std_logic_vector(WORD_SIZE - 1 downto 0);
-         i_D1         : in std_logic_vector(WORD_SIZE - 1 downto 0);
-         o_O          : out std_logic_vector(WORD_SIZE - 1 downto 0));
-  end component;
-
-  component dffg_N
-    port(i_CLK        : in std_logic;     -- Clock input
-         i_RST        : in std_logic;     -- Reset input
-         i_WE         : in std_logic;     -- Write enable input
-       	 i_D          : in std_logic_vector( WORD_SIZE - 1 downto 0);     -- Data value input
-       	 o_Q          : out std_logic_vector( WORD_SIZE - 1 downto 0));   -- Data value output
-  end component;
-
-  signal instruction_offset : std_logic_vector( WORD_SIZE - 1 downto 0):=  to_stdlogicvector(x"00000004");  
-  signal program_counter : std_logic_vector( ADDR_LEN - 1 downto 0);
+  o_memToReg <= '1' when op_code = DECODE_OP(lwc)
+                else '0';
   
-  signal next_address : std_logic_vector( ADDR_LEN - 1 downto 0);
-  
-  signal next_instruction : std_logic_vector( ADDR_LEN - 1 downto 0);
-  
-  signal jump_address : std_logic_vector( ADDR_LEN - 1 downto 0);
-  
-  signal branch_address : std_logic_vector( WORD_SIZE - 1 downto 0) := to_stdlogicvector(x"00000000");
-  signal branch_immediate : std_logic_vector( WORD_SIZE - 1 downto 0);
-  
-  
-  signal branch_or_register : std_logic_vector( WORD_SIZE - 1 downto 0);
-  signal branch_jump : std_logic_vector( WORD_SIZE - 1 downto 0);
-  
-begin
-  
-  branch_immediate(17 downto 2) <= i_imm(15 downto 0);
-  
-  PC: dffg_N
-  port map(
-    i_CLK => i_clk,
-    i_RST => '0',
-    i_WE => '1',
-    i_D => next_address,
-    o_Q => program_counter
-  );
+  -- TODO ALU OP
 
-  program_plus_four: Ripple_Adder
-  port map(
-            i_A    => instruction_offset,
-		      	i_B    => program_counter,
-            o_S    => next_instruction);
-
-
-  branch_address_calc: Ripple_Adder
-  port map(
-            i_A    => next_instruction,
-		      	i_B    => branch_immediate,
-            o_S    => branch_address);
-		   
-	jump_address( 31 downto 28) <= program_counter( 31 downto 28);
-	jump_address( 27 downto 2) <= instruction( 25 downto 0);
-	jump_address( 1 downto 0) <= "00";
-	
-
-  Branch_Reg: mux2t1_N
-  port map(
-            i_S     => j_type(3),
-            i_D0    => branch_address,
-            i_D1    => i_addr,
-            o_O     => branch_or_register);
-
-  branch_jump_calc: mux2t1_N
-  port map(
-            i_S     => j_type(2),
-            i_D0    => jump_address,
-            i_D1    => branch_or_register,
-            o_O     => branch_jump);
-
-  next_address_calc: mux2t1_N
-  port map(
-            i_S     => j_type(1),
-            i_D0    => next_instruction,
-            i_D1    => branch_jump,
-            o_O     => next_address);	
-
+  o_ALUSrc <= '1' when DECODE_OP(r_type) & op_code = DECODE_OP(addic)
+                else '1' when DECODE_OP(r_type) & op_code = DECODE_OP(addiuc)
+                else '1' when DECODE_OP(r_type) & op_code = DECODE_OP(andic)
+                else '1' when DECODE_OP(r_type) & op_code = DECODE_OP(luic)
+                else '1' when DECODE_OP(r_type) & op_code = DECODE_OP(xori)
+                else '1' when DECODE_OP(r_type) & op_code = DECODE_OP(oric)
+                else '1' when DECODE_OP(r_type) & op_code = DECODE_OP(sltic)
+                else '0'; 
+  
+  o_jumpIns <= '1' when op_code = DECODE_OP(jrc)
+                else '0';
+  
 end structure;
