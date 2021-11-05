35caa08ac088d40aae4cac9aa2cb9a45af17e72b
Liz Fransen <32855802+elizabethlfransen@users.noreply.github.com>
2021-11-05 10:49:06 -0500

    Remove syscalls

--- Test Files/Proj1_base_test.s
+++ Test Files/Proj1_base_test.s
@@ -6,74 +6,72 @@ main:
     # s1 => 1
     addiu $s1, $zero, 1
     # s2 => 2
-    addiu $s2, $zero, 2
+    # addiu $s2, $zero, 2
     
     # expect 1 + 2 = 3
     add $a0, $s1, $s2
-    jal print
+    # jal print
     
     # expect 3 + -1 = 2
     addi $a0, $a0, -1
-    jal print
+    # jal print
     
     # expect an overflow
     addiu $a0, $zero, 0x7FFFFFFF
     addiu $a0, $a0, 1
-    jal print
+    # jal print
     
     # expect 2
     addu $a0, $s1, $s1
-    jal print
+    # jal print
     
     # expect 0
     and $a0, $s1, $s2
-    jal print
+    # jal print
     
    # expect 2
     andi $a0, $s2, 3
-    jal print
+    # jal print
     
     # expect $a0 = 1 nor 2 = 0xFFFFFFFC
     nor $a0, $s1, $s2
-    jal print
+    # jal print
     
     
     # expect 1
     slt $a0, $s1, $s2
-    jal print
+    # jal print
     
     #expect 0
     slti $a0, $s1, 0
-    jal print
+    # jal print
     
     # expect 0
     sltiu $a0, $s2, 2
-    jal print
+    # jal print
     
     # shifting
     # expect 2
     sll $a0, $s1, 1
-    jal print
+    # jal print
     
     #expect 0
     srl $a0, $s2, 2
-    jal print
+    # jal print
     
     #expect 1 - 2 = -1
     sub $a0, $s1, $s2
-    jal print
+    # jal print
     
     # expect 1 - 1 = 0
     subu $a0, $s1, $s1
-    jal print
+    # jal print
     
     # expect 02020202
     REPL.QB $a0, 2
 
     # Exit program
-exit:  
-    li $v0, 10
-    syscall
+    j exit
     
 print:
     # Print number
@@ -88,3 +86,4 @@ print:
     # restore argument
     or $a0, $t0, $zero
     jr $ra
+exit:
